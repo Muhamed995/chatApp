dfb957c18a471ec58ee4e19d54dd7103ccd1cdd6
Landing page <muhamed.halilovic@hotmail.com>
2018-11-11 00:56:59 +0100

    new commit

--- chat.js
+++ chat.js
@@ -1,57 +1,64 @@
-
-let ul=document.querySelector('#userMessage');
-ul.innerHTML='';
-let writeMessage = function (msg,klasa){
-    let li=' <li class="'+klasa+'">';
-    li+=msg;
-    li+='</li>';
+const socket = new WebSocket("ws://localhost:" + sessionStorage.port);
+let ul = document.querySelector('#userMessage');
+let cnt = 0;
+let writeMessage = function (msg, klasa) {
+    let li = ' <li tabIndex="-1" class="' + klasa + '">';
+    li += msg;
+    li += '</li>';
     ul.innerHTML += li;
+    ul.lastChild.focus();
+    document.querySelector('#msg').focus();
 }
-const socket = new WebSocket("ws://localhost:"+ sessionStorage.port);
-document.querySelector('#send').addEventListener("click",(e)=>{
+let sendFunction =  (e) => {
     let poruka = document.querySelector('#msg');
     socket.send(poruka.value);
-    writeMessage(poruka.value,"msgRight");
-    poruka.value='';
-},false);
+    writeMessage(poruka.value, "msgRight");
+    poruka.value = '';
+};
 
+ul.innerHTML = '';
 
-// socket.close()
-socket.onopen = (e) =>{
+document.querySelector('#send').addEventListener("click",sendFunction, false);
+document.addEventListener("keypress",(e)=>{
+    if(e.keyCode == 13)
+        sendFunction(e);
+}, false);
+document.querySelector('#backTo').addEventListener("click",(e)=>{socket.close();}, false); 
+
+socket.onopen = (e) => {
     console.log("connected");
     document.querySelector("#localName").innerHTML = sessionStorage.name;
     document.getElementById("localConnected").innerHTML = "Connected";
     document.getElementById("localConnected").className = 'connected';
 };
 
-socket.onclose = (e)=>{
+socket.onclose = (e) => {
     console.log("connection closed")
+    window.location = "login.html";
+};
+socket.onerror = (e) => {
+    console.log("error  " + e.data)
+    socket.close();
 };
 
 
-socket.onerror =(e)=>{
-    console.log("error  "+e.data)
-};
-
-socket.onmessage =(e)=>{
-    if (e.data.indexOf('!cnc!') !== -1){
+socket.onmessage = (e) => {
+    if (e.data.indexOf('!cnc!') !== -1) {
         let name = e.data.replace("!cnc!", "");
-        document.querySelector("#remoteName").innerHTML= name;
+        document.querySelector("#remoteName").innerHTML = name;
         document.getElementById("remoteConnected").innerHTML = "Connected";
         document.getElementById("remoteConnected").className = 'connected';
-    }
-    else{
-        writeMessage(e.data,"msgLeft");
+    } else {
+        writeMessage(e.data, "msgLeft");
         console.log("message recieved");
     }
 };
 
-let cnt = 0;
-setInterval(()=>{
-    cnt++;
-    if(cnt<3){
-        socket.send(sessionStorage.name + "!cnc!"); 
-    }else{
+setInterval(() => {
+    if (cnt < 5) {
+        socket.send(sessionStorage.name + "!cnc!");
+        cnt++;
+    } else {
         console.log('timeout');
     }
-},2000)
+}, 2000)
--- login-validation.js
+++ login-validation.js
@@ -1,47 +1,34 @@
-
-let validate =function(){
+let validate = function () {
     let username = document.getElementById('username').value;
-let port = document.getElementById('port').value;
-let valid = true;
-console.log(username);
+    let port = document.getElementById('port').value;
+    let valid = true;
+    if (username == '') {
+        document.getElementById('message').innerHTML = 'Username is required';
+        valid = false;
+    } else {
+        document.getElementById('message').innerHTML = '';
+    }
+    if (port >= 65545 || port == '') {
+        document.getElementById('message2').innerHTML = 'Port is out of range';
+        valid = false;
+    } else {
+        document.getElementById('message2').innerHTML = '';
+    }
 
-if(username == '' ){
-    document.getElementById('message').innerHTML ='Username is required';
-    valid=false;
-   
-    
-}else{
-    document.getElementById('message').innerHTML ='';
-    
+    if (valid) {
+        let socket = new WebSocket('ws://127.0.0.1:' + port);
+        let used = (e) => {
+            document.getElementById('message2').innerHTML = 'Port in use or inactive';
+            console.log(e);
+        };
+        socket.onerror = used;
+        socket.onopen = (e) => {
+            sessionStorage.setItem("port", port);
+            sessionStorage.setItem("name", username);
+            socket.close();
+            setTimeout(() => {
+                window.location = "chatbox.html"
+            }, 1000);
+        };
+    }
 }
-
-let socket;
-
-if(port >=65545 || port == ''){
-    document.getElementById('message2').innerHTML ='Port is out of range';
-    valid=false;
-}else{
-   
-    document.getElementById('message2').innerHTML ='';
-    
-}
-
-if(valid){
-    socket = new WebSocket('ws://127.0.0.1:'+ port);
-    let used = (e)=>{
-        document.getElementById('message2').innerHTML ='Port in use or inactive';
-    };
-    socket.onerror=used;
-    //socket.onclose=used;
-    socket.onopen=(e)=>{
-    sessionStorage.setItem("port", port);
-    sessionStorage.setItem("name", username);
-    socket.close();
-    setTimeout(()=>{window.location="chatbox.html"},1000);
-    };
-    
-
-}
-}
-
-
